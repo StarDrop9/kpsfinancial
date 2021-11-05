c2a4acad2f1cdeadcc6ea10195aa8d780b316299
Kurt Starck <kurtstarck@yahoo.com>
2021-11-05 16:16:20 -0500

    new file:   .env 	new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/components/ButtonElements.js 	new file:   src/components/Footer/FooterElements.js 	new file:   src/components/Footer/index.js 	new file:   src/components/HeroSection/HeroElements.js 	new file:   src/components/HeroSection/index.js 	new file:   src/components/InfoSection/Data.js 	new file:   src/components/InfoSection/InfoElements.js 	new file:   src/components/InfoSection/index.js 	new file:   src/components/Navbar/NavbarElements.js 	new file:   src/components/Navbar/index.js 	new file:   src/components/ScrollToTop.js 	new file:   src/components/Services/ServicesElements.js 	new file:   src/components/Services/index.js 	new file:   src/components/Sidebar/SidebarElements.js 	new file:   src/components/Sidebar/index.js 	new file:   src/components/SignIn/SigninElements.js 	new file:   src/components/SignIn/index.js 	new file:   src/images/svg-1.svg 	new file:   src/images/svg-2.svg 	new file:   src/images/svg-3.svg 	new file:   src/images/svg-4.svg 	new file:   src/images/svg-5.svg 	new file:   src/images/svg-6.svg 	new file:   src/index.js 	new file:   src/pages/index.js 	new file:   src/pages/signin.js 	new file:   src/videos/vecteezy_the-increase-in-stock-market-earnings-displayed-in-blue-screen-graphics_1625978.mov 	new file:   src/videos/video.mov 	new file:   src/videos/video.mp4 	new file:   yarn.lock

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElements';
+
+import Video from '../../videos/video.mov';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='video/mov' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Options Trading made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $10 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
